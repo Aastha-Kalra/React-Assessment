0ad7b51e28e512a055fcad2be401fb2f5520a5f4
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-10 22:56:49 +0530

    Question1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Question1 from "./components/Question1";
+function App() {
+  return (
+    <div className="bg-black min-h-screen py-5 text-green-500">
+      <Question1 />
+    </div>
+  );
+}
+
+export default App;
