c82db7eb7dee67ec6d18a962747705f4f84ac9cd
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-11 12:25:22 +0530

    question3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Question1 from "./components/Question1";
+import Question3 from "./components/Question3/Question3";
 function App() {
   return (
     <div className="bg-black min-h-screen py-5 text-green-500">
-      <Question1 />
+      {/* <Question1 /> */}
+      <Question3/>
     </div>
   );
 }
