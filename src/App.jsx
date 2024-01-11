a90a878aa0340d8b379137a837de858466ecce81
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-11 15:56:08 +0530

    shift question1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Question1 from "./components/Question1";
-import Question3 from "./components/Question3/Question3";
+import Question1 from './components/Question1/Question1';
+import Question2 from './components/Question2/Question2';
+import Question3 from './components/Question3/Question3';
+import Question4 from "./components/Question4/Question4";
 function App() {
   return (
     <div className="bg-black min-h-screen py-5 text-green-500">
       {/* <Question1 /> */}
-      <Question3/>
+      {/* <Question2/> */}
+      {/* <Question3/> */}
+      <Question4/>
     </div>
   );
 }
