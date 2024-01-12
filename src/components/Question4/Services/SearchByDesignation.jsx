b62a818a8454b3362a2cbd6fccdbe2c5e3e7bc21
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-12 16:29:39 +0530

    formatted

--- src/components/Question4/Services/SearchByDesignation.jsx
+++ src/components/Question4/Services/SearchByDesignation.jsx
@@ -1,30 +1,32 @@
-// services/SearchByDesignation.js
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SearchByDesignation = ({ employees, setFilteredEmployees }) => {
-  const [searchDesignation, setSearchDesignation] = useState('');
+  const [searchDesignation, setSearchDesignation] = useState("");
 
   const handleSearchByDesignation = () => {
     const filteredEmployees = employees.filter(
-      (employee) => employee.position.toLowerCase() === searchDesignation.toLowerCase()
+      (employee) =>
+        employee.position.toLowerCase() === searchDesignation.toLowerCase()
     );
     setFilteredEmployees(filteredEmployees);
   };
 
   return (
     <div>
- 
-  
+      <input
+        type="text"
+        value={searchDesignation}
+        onChange={(e) => setSearchDesignation(e.target.value)}
+        placeholder="Search by Designation"
+        className="bg-slate-200 py-2 px-2 rounded-l-lg focus:outline-none"
+      />
 
-        <input
-          type="text"
-          value={searchDesignation}
-          onChange={(e) => setSearchDesignation(e.target.value)}
-          placeholder='Search by Designation'
-          className='bg-slate-200 py-2 px-2 rounded-l-lg focus:outline-none'
-        />
-    
-      <button className='bg-green-300 text-green-900 px-4 py-2 rounded-r-lg' onClick={handleSearchByDesignation}>Search</button>
+      <button
+        className="bg-green-300 text-green-900 px-4 py-2 rounded-r-lg"
+        onClick={handleSearchByDesignation}
+      >
+        Search
+      </button>
     </div>
   );
 };
--- src/components/Question4/Services/SearchByName.jsx
+++ src/components/Question4/Services/SearchByName.jsx
@@ -1,29 +1,32 @@
-// services/SearchByName.js
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SearchByName = ({ employees, setFilteredEmployees }) => {
-  const [searchName, setSearchName] = useState('');
+  const [searchName, setSearchName] = useState("");
 
   const handleSearchByName = () => {
-    const filteredEmployees = employees.filter(
-      (employee) => employee.name.toLowerCase().includes(searchName.toLowerCase())
+    const filteredEmployees = employees.filter((employee) =>
+      employee.name.toLowerCase().includes(searchName.toLowerCase())
     );
     setFilteredEmployees(filteredEmployees);
   };
 
   return (
     <div>
-     
       <label>
         <input
           type="text"
           value={searchName}
           onChange={(e) => setSearchName(e.target.value)}
-          placeholder='Search by Name'
-          className='bg-slate-200 py-2 px-2 rounded-l-lg focus:outline-none'
+          placeholder="Search by Name"
+          className="bg-slate-200 py-2 px-2 rounded-l-lg focus:outline-none"
         />
       </label>
-      <button className='bg-green-300 text-green-900 px-4 py-2 rounded-r-lg' onClick={handleSearchByName}>Search</button>
+      <button
+        className="bg-green-300 text-green-900 px-4 py-2 rounded-r-lg"
+        onClick={handleSearchByName}
+      >
+        Search
+      </button>
     </div>
   );
 };
