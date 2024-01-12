9071bfd5edf1b1c262e91b85761ccd3f5de097c4
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-12 23:45:45 +0530

    added routing

--- src/components/Question4/Question4.jsx
+++ src/components/Question4/Question4.jsx
@@ -1,20 +1,29 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import Login from './Login';
-import Dashboard from './Dashboard';
+import { BrowserRouter , Route, Routes } from "react-router-dom";
+import AddEmployee from './AddEmployee'
+import ViewEmployees from './ViewEmployee'
 const Question4 = ({}) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [username, setUsername] = useState('Admin');
-  
+    const [employees, setEmployees] = useState([]);
     const handleLogin = () => {
       setIsLoggedIn(true);
     };
+    useEffect(()=>{
+      isLoggedIn? setUsername(username):setUsername(null)
+    })
   return (
     <div>
-    {isLoggedIn ? (
-      <Dashboard username={username} />
-    ) : (
-      <Login onLogin={handleLogin} />
-    )}
+   
+   <BrowserRouter>
+   <Routes>
+   <Route exact path="/"  element={<Login onLogin={handleLogin} />}/>
+     <Route exact path="/add"   element={<AddEmployee setEmployees={setEmployees}/>} />
+     <Route exact path="/view" element={<ViewEmployees employee={employees}/>} />
+   </Routes>
+ </BrowserRouter>
+   
   </div>
   )
 }
