bdd1f4afa5eac9c238f1bc278b49f4ee9e6777fa
Aastha kalra <aasthakalra2004@gmail.com>
2024-01-13 15:56:20 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
 import Question4 from "./components/Question4/Question4";
 function App() {
   return (
     <div className="bg-black min-h-screen py-5 text-green-500">
-      <Question4 />
+  <BrowserRouter>
+  <Question4 />
+  </BrowserRouter>
     </div>
   );
 }
--- src/components/Question4/Question4.jsx
+++ src/components/Question4/Question4.jsx
@@ -1,31 +1,71 @@
-import React,{useEffect, useState} from 'react'
-import Login from './Login';
-import { BrowserRouter , Route, Routes } from "react-router-dom";
-import AddEmployee from './AddEmployee'
-import ViewEmployees from './ViewEmployee'
-const Question4 = ({}) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [username, setUsername] = useState('Admin');
-    const [employees, setEmployees] = useState([]);
-    const handleLogin = () => {
-      setIsLoggedIn(true);
-    };
-    useEffect(()=>{
-      isLoggedIn? setUsername(username):setUsername(null)
-    })
+import React, { useState } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import Login from "./Login";
+import AddEmployee from "./AddEmployee";
+import ViewEmployees from "./ViewEmployee";
+
+const Question4 = () => {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [employees, setEmployees] = useState([]);
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
   return (
     <div>
+         <nav className="flex justify-center items-center">
+         {
+           !isLoggedIn? null:  <ul className="flex justify-center items-center gap-5">
+           <li>
+             <Link
+               to="/add"
+               onClick={() => {
+                 if (!isLoggedIn) alert("please login first ");
+                 navigate("/");
+               }}
+             >
+               Add Employee
+             </Link>
+           </li>
+           <li>
+             <Link to="/view"  onClick={() => {
+                 if (!isLoggedIn) alert("please login first ");
+                 navigate("/");
+               }}>View Employees</Link>
+           </li>
+         </ul>
+         }
+      
+      </nav>
+      <Routes>
+        {!isLoggedIn ? (
+          <>
+            <Route path="/*" element={<Login onLogin={handleLogin} />} />
+          </>
+        ) : (
+          <>
+            <Route
+              exact
+              path="/add"
+              element={<AddEmployee setEmployees={setEmployees} />}
+            />
+            <Route
+              exact
+              path="/view"
+              element={<ViewEmployees employees={employees} />}
+            />
+          </>
+        )}
+      </Routes>
    
-   <BrowserRouter>
-   <Routes>
-   <Route exact path="/"  element={<Login onLogin={handleLogin} />}/>
-     <Route exact path="/add"   element={<AddEmployee setEmployees={setEmployees}/>} />
-     <Route exact path="/view" element={<ViewEmployees employee={employees}/>} />
-   </Routes>
- </BrowserRouter>
-   
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Question4
+export default Question4;
